352231ea9b2624c5ce67f7fee656c5039dc5da06
Gabse213 <sg2697@mci4me.at>
2025-06-05 21:47:07 +0200

    Zwischenspeicherung nach LV 05.06.25

---
+++ ekgdata.py
@@ -0,0 +1,61 @@
+import json
+import pandas as pd
+import plotly.express as px
+#from person import load_by_id, calc_max_heart_rate, calc_age
+
+# Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
+
+class EKGdata:
+
+## Konstruktor der Klasse soll die Daten einlesen
+
+    def __init__(self, ekg_dict):
+        #pass
+        self.id = ekg_dict["id"]
+        self.date = ekg_dict["date"]
+        self.data = ekg_dict["result_link"]
+        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['Messwerte in mV','Zeit in ms',])
+        self.df = self.df.iloc[:5000]  # Entferne die erste Zeile, da sie nur die Spaltennamen enthält
+
+
+    def plot_time_series(self):
+
+        # Erstellte einen Line Plot, der ersten 2000 Werte mit der Zeit aus der x-Achse
+        self.fig = px.line(self.df.head(2000), x="Zeit in ms", y="Messwerte in mV")
+        #return self.fig 
+
+    def find_peaks(self, threshold, respacing_factor=5):
+        series=self.df['Messwerte in mV']
+    # Respace the series
+        series = series.iloc[::respacing_factor]
+    
+    # Filter the series
+        series = series[series>threshold]
+
+
+        peaks = []
+        last = 0
+        current = 0
+        next = 0
+
+        for index, row in series.items():
+            last = current
+            current = next
+            next = row
+
+            if last < current and current > next and current > threshold:
+                peaks.append(index-respacing_factor)
+
+        return peaks
+
+
+if __name__ == "__main__":
+    print("This is a module with some functions to read the EKG data")
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    ekg_dict = person_data[0]["ekg_tests"][0]
+    print(ekg_dict)
+    ekg = EKGdata(ekg_dict)
+    print(ekg.df.head())
+    peaks=ekg.find_peaks(340)
+    print(peaks)
